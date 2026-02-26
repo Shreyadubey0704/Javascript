13485c9e38620a27a5a2148d3b61d36e134f34e1
Shreya Dubey <shreyadubey0704@gmail.com>
2026-02-26 20:42:38 +0530

    lec3

---
+++ lec3/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all Js code as newer version
+
+//alert(3+3) // we are using node js , not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Shreya")
+
+let name = "Shreya"
+let age = 18
+let isloggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof undefined);
+console.log(typeof null);
+
