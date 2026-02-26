a3d728a66827552f0baf8c1dfb8074651c1fdb38
Shreya Dubey <shreyadubey0704@gmail.com>
2026-02-26 17:30:17 +0530

    lec2

---
+++ lec2/Variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "shreya@gmail.com."
+var accountPasswod = "45634"
+accountCity = "Jaipur"
+let accountState;//output is undefined 
+
+//accountId = 2 // not allowed
+
+
+accountEmail = "shdh@djfdh"
+accountPasswod = "7656"
+accountCity ="Amroha"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPasswod,accountCity,accountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope.
+*/
