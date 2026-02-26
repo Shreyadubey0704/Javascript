3395c1a9c9912514a0603c5596859770aa5ffaa3
Shreya Dubey <shreyadubey0704@gmail.com>
2026-02-27 01:23:05 +0530

    lec4

--- lec3/datatypes.js
+++ lec3/datatypes.js
@@ -26,6 +26,6 @@ let state;
 
 console.log(typeof age);
 console.log(typeof name);
-console.log(typeof undefined);
-console.log(typeof null);
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
 
---
+++ lec4/conversionandoperations.js
@@ -0,0 +1,39 @@
+let score = "Shreya"
+//let score = "33"
+// //   "33" ek valid number string hai,Isliye JavaScript usko convert kar deta hai:
+// "33"=>33 
+//let score = "33abc"
+//let score = true
+
+
+
+console.log(typeof score);
+console.log(typeof(score)); // Parentheses optional hain
+
+
+let valueIntNumber = Number(score)
+//console.log(typeof valueIntNumber);
+//console.log( valueIntNumber);
+
+// "33" => 33
+// "33abc" => NaN (not an number)
+//  true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn )
+console.log(booleanIsLoggedIn);
+ 
+// 1 => true; 0 => false
+// "" => false
+// "Shreya" => true
+
+let somenumber = 33 
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+
+
+
+
+
