951398ba3cee6135ed6ef6888694af9378cd9ef6
Shreya Dubey <shreyadubey0704@gmail.com>
2026-03-31 00:56:11 +0530

    lec8

---
+++ lec7/Stringinjs.js
@@ -0,0 +1,37 @@
+const name = "Shreya"
+const repoCount = 50
+
+//console.log(name + repoCount +  " Value "); not use at this morden time
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`  );
+
+
+//declare string
+const gameName = new String('shreya-dubey') 
+console.log(gameName [0]);
+console.log(gameName.__proto__);
+
+
+//Strings ke method.............orr bhi h mdn pr padhe...
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString); 
+
+const newStringOne = "  Shreya    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+ 
+const url = "https://hitesh.com/hitesh%20chodhary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'))
