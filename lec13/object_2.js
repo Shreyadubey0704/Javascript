fb6c9025e47abf31ce70883615d55407436a82f0
Shreya Dubey <shreyadubey0704@gmail.com>
2026-04-04 00:25:51 +0530

    objects destructuring and JSON API

--- lec13/object_2.js
+++ lec13/object_2.js
@@ -6,4 +6,85 @@ const tinderUser = {} // non singleton object h
  tinderUser.isLoggedIn = false 
 
 
+//console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gamil.com",
+    fullname: {
+        userfullname :{
+            firstname : "Shreya",
+            lastname : "Dubey"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {1:"a",2:"b"}
+
+//const obj3 = {obj1 ,obj2}
+//const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email : "h@gmail.com"
+    },
+    {
+        id : 1,
+        email : "h@gmail.com"
+    },
+    {
+        id : 1,
+        email : "h@gmail.com"
+    }
+]
+
+users[1].email
 console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    couresename: "js in hindi",
+    price :"999",
+    courseInstructor : "hitesh bhai"
+}
+
+course.courseInstructor
+
+const{courseInstructor: Instructor} = course
+
+//console.log(courseInstructor);
+console.log(Instructor);
+
+
+//react (destructuring )
+// const navbar = (company ) => {
+
+// }
+// navbar(company = "hitesh")
+
+
+// API json
+
+// 
+
+{
+    name : "hitesh",
+    coursename : "js in hindi",
+    "price": "freee"
+}
+
+[
+    {},
+    {},
+    {}
+]
