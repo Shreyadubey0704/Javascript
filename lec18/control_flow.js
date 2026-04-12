222bb0061452c718ccfc26d8057b85e284c85d74
Shreya Dubey <shreyadubey0704@gmail.com>
2026-04-12 23:32:51 +0530

    control flow

---
+++ lec18/control_flow.js
@@ -0,0 +1,12 @@
+// if
+
+const isUserloggedIn = true 
+const temperature = 41 
+
+if(temperature < 50 ){
+    console.log("less than 50");
+    
+}
+console.log("temperature is greater than 50");
+
+// < ,>, <= ,>= , == , != , === (data type bhi check krta h) ,!== (for anti pattern check)
